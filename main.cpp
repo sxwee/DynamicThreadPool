dc1fbb81d69369109478ba50cf0173902df7e02f
lw <we.luo@foxmail.com>
2023-02-28 19:53:56 +0800

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,9 +4,14 @@
 #include <algorithm>
 
 using namespace std;
+using namespace dpool;
+
+std::mutex coutMtx;
 
 int compute(int *arr, int start, int end)
 {
+    cout << "thread_id: " << std::this_thread::get_id();
+    cout << " [" << start << " , " << end << "]" << endl;
     int res = 0;
     for (int i = start; i <= end; ++i)
     {
@@ -23,9 +28,14 @@ int main()
     for (int i = 0; i < n; i++)
         arr[i] = rand() % 10;
     int thread_nums = 10;
-    int task_nums = 5;
-    dpool::ThreadPool pool(thread_nums);
-
+    int task_nums = 6;
+    ThreadPool pool(thread_nums);
+    // 首先创建3个线程
+    for (int i = 0; i < 3; i++)
+    {
+        pool.createNewThread();
+    }
+    // 提交任务
     int k = n / task_nums;
     int ans = 0;
     for (int i = 1; i <= task_nums; ++i)
@@ -35,7 +45,7 @@ int main()
         auto fut = pool.submit(compute, ref(arr), start, end - 1);
         int seg_sum = fut.get();
         ans += seg_sum;
-        cout << "[" << start << " , " << end << "]: " << seg_sum << endl;
+        cout << "thread nums: " << pool.threadsNum() << endl;
     }
     int rans = 0;
     for (int i = 0; i < n; i++)
